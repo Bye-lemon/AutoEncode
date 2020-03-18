4cf7b94e07c87b2d0104bc44b730a49294aaeb40
Yqzzxlj <yq736677@icloud.com>
2020-03-18 15:01:10 +0800

    move test on cpu

--- get_bits.py
+++ get_bits.py
@@ -9,23 +9,22 @@ def get_bit(num_train, num_test, TARGET_DIM, anchor_loader, test_loader, encoder
 
   testY = torch.empty(num_test, TARGET_DIM)
 
+  encoder.cpu()
+
+
   print('making trainY')
   for step, (x, _) in tqdm(enumerate(anchor_loader)):
-    x = x.cuda()
     encoded = encoder(x)
-    del x
     trainY[(step*BATCH_SIZE):((step+1)*BATCH_SIZE)] = encoded
 
   print('making testY')
   for step, (x, y) in enumerate(test_loader):
-    x = x.cuda()
     encoded = encoder(x)
-    del x
     testY[(step*BATCH_SIZE):((step+1)*BATCH_SIZE)] = encoded
 
 
-  trainY = trainY.detach().cpu().numpy()
-  testY = testY.detach().cpu().numpy()
+  trainY = trainY.detach().numpy()
+  testY = testY.detach().numpy()
 
   trainY = compactBit(trainY)
   testY = compactBit(testY)
