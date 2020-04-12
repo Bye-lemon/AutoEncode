f9a626359c5ae90d26ceeebaa3760a35161207bd
Yqzzxlj <yq736677@icloud.com>
2020-04-12 19:17:32 +0800

    save

--- model.py
+++ model.py
@@ -63,7 +63,58 @@ class Decoder(nn.Module):
     x = self.main(x)
     return x
 
+
+
 class ResEncoder(nn.Module):
+  def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                    padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+  def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+  class BasicBlock(nn.Module):
+    expansion = 1
+    __constants__ = ['downsample']
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                base_width=64, dilation=1, norm_layer=None):
+      super(BasicBlock, self).__init__()
+      if norm_layer is None:
+          norm_layer = nn.BatchNorm2d
+      if groups != 1 or base_width != 64:
+          raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+      if dilation > 1:
+          raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+      # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+      self.conv1 = conv3x3(inplanes, planes, stride)
+      self.bn1 = norm_layer(planes)
+      self.relu = nn.ReLU(inplace=True)
+      self.conv2 = conv3x3(planes, planes)
+      self.bn2 = norm_layer(planes)
+      self.downsample = downsample
+      self.stride = stride
+
+    def forward(self, x):
+      identity = x
+
+      out = self.conv1(x)
+      out = self.bn1(out)
+      out = self.relu(out)
+
+      out = self.conv2(out)
+      out = self.bn2(out)
+
+      if self.downsample is not None:
+          identity = self.downsample(x)
+
+      out += identity
+      out = self.relu(out)
+
+      return out
   def __init__(self, n_z, n_channel=1, dim_h=128):
     super(ResEncoder, self).__init__()
 
@@ -80,13 +131,7 @@ class ResEncoder(nn.Module):
       nn.ReLU(True),
     )
     # 7 * 7 -> 7 * 7
-    self.block2 = nn.Sequential(
-      nn.Conv2d(self.dim_h * 2, self.dim_h * 2, 3, 1, 1, bias=False),
-      nn.BatchNorm2d(self.dim_h * 2),
-      nn.ReLU(True),
-      nn.Conv2d(self.dim_h * 2, self.dim_h * 2, 3, 1, 1, bias=False),
-      nn.BatchNorm2d(self.dim_h * 2),
-    )
+    self.layer1 = self._make_layer(BasicBlock, self.dim_h, 3)
     self.relu = nn.ReLU(True)
     self.fc = nn.Sequential(
       nn.Linear(self.dim_h * 2 * 7 * 7, self.n_z),
@@ -95,14 +140,35 @@ class ResEncoder(nn.Module):
 
   def forward(self, x):
     x = self.block1(x)
-    out = self.block2(x)
-    out += x
+    out = self.layer1(x)
     out = self.relu(out)
     out = out.view(-1, self.dim_h * 2 * 7 * 7)
     out = self.fc(out)
-    print(out)
-    input()
     return out 
+
+  def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+    norm_layer = self._norm_layer
+    downsample = None
+    previous_dilation = self.dilation
+    if dilate:
+      self.dilation *= stride
+      stride = 1
+    if stride != 1 or self.inplanes != planes * block.expansion:
+      downsample = nn.Sequential(
+          conv1x1(self.inplanes, planes * block.expansion, stride),
+          norm_layer(planes * block.expansion),
+      )
+
+    layers = []
+    layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                        self.base_width, previous_dilation, norm_layer))
+    self.inplanes = planes * block.expansion
+    for _ in range(1, blocks):
+      layers.append(block(self.inplanes, planes, groups=self.groups,
+                          base_width=self.base_width, dilation=self.dilation,
+                          norm_layer=norm_layer))
+
+    return nn.Sequential(*layers)
 
 class ResDecoder(nn.Module):
   def __init__(self, n_z, n_channel=1, dim_h=128):
